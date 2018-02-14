0d3b6c966a931db0fb29ca70beb8a33decd0d027
borsim2 <mb16240@bristol.ac.uk>
2018-02-14 10:19:59 +0000

    Scheduler added to file-upload, ready for mosaicmaker

--- file-upload/complete/src/main/java/hello/Application.java
+++ file-upload/complete/src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import it.sauronsoftware.cron4j.Scheduler;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +17,20 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        Scheduler s = new Scheduler();
+        // TODO put these 2 lines in once mosaiccreator is imported somehow. 
+        // Rename "main" in Mosaic.java to public void run() {...} and use public class Mosaic implements Runnable {...}
+        //Mosaic m = new Mosaic();
+        //s.schedule("0 0 * * *", m);
+
+        // Schedule a once-a-day task
+        s.schedule("* * * * *", new Runnable() {
+            public void run() {
+                System.out.println("Another minute ticked away...");
+            }
+        });
+        s.start();
     }
 
     @Bean
