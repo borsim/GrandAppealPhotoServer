9dba65fbe792c18bbbcd17debab7414dedd3c7b6
borsim2 <mb16240@bristol.ac.uk>
2018-03-14 10:42:45 +0000

    Crop and resize ready to be tested

--- mosaiccreator/mosaic3/src/Main.java
+++ mosaiccreator/mosaic3/src/Main.java
@@ -1,8 +1,10 @@
 package uk.org.codebox.mosaic;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		Mosaic mosaic = new Mosaic("tiles", "input.jpg", "output.jpg", 320, 240, 4, false, 2);
-		mosaic.run();
+		mosaic.resizeAndCropImage("CONTOUR_WORKS.png");
 	}
 }
